16795ab75fa0c3e9c10178f1afae872407cde3b7
Dave Kelsey <David.Kelsey@gov.bc.ca>
2020-04-13 15:08:17 -0700

    add env vars

--- app.sh
+++ app.sh
@@ -1,3 +1,4 @@
 FLASK_APP=$PWD/app/http/api/endpoints.py \
-     FLASK_ENV=development \
-      python -m flask run --port 4433
+    FLASK_ENV=${ENV:-development} \
+    PORT=${APIPORT:-8080}  \
+    python -m flask run --port ${PORT}
