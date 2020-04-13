ef4581ce507b6da0c2f88c122ea839e4b6625a7e
Dave Kelsey <David.Kelsey@gov.bc.ca>
2020-04-13 15:10:42 -0700

    add export

--- app.sh
+++ app.sh
@@ -1,4 +1,5 @@
-FLASK_APP=$PWD/app/http/api/endpoints.py \
+export FLASK_APP=$PWD/app/http/api/endpoints.py \
     FLASK_ENV=${ENV:-development} \
-    PORT=${APIPORT:-8080}  \
-    python -m flask run --port ${PORT}
+    PORT=${APIPORT:-8080}
+
+python -m flask run --port ${PORT}
