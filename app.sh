e35d2702e58487445ddacb6c2d2e5942de5e9e2f
Dave Kelsey <David.Kelsey@gov.bc.ca>
2020-04-15 15:55:55 -0700

    change listen host

--- app.sh
+++ app.sh
@@ -2,4 +2,4 @@ export FLASK_APP=$PWD/app/http/api/endpoints.py \
     FLASK_ENV=${ENV:-development} \
     PORT=${APIPORT:-8080}
 
-python -m flask run --port ${PORT}
+python -m flask run --host 0.0.0.0 --port ${PORT}
